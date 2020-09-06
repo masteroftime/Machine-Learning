4333b0a15192fdbda464dbfcee8fea4f22782543
masteroftime <martinfeiler@hotmail.com>
2020-09-06 13:51:38 +0000

    Converted Q-Learning Algorithm to class

--- main.py
+++ main.py
@@ -2,77 +2,91 @@ import numpy as np
 from random import random, randrange
 import gym
 
-
-training_epochs = 1000
 epsilon = 0.1  # parameter for epsilon-greedy policy
-alpha = 0.1    # learning rate
-gamma = 1      # discounting factor
+alpha = 0.1  # learning rate
+gamma = 1  # discounting factor
 
-env = gym.make('Blackjack-v0')
 
-# q-table for all the states:
+class QAgent:
+    def __init__(self, env, dims, map_func=None):
+        self.env = env
+        self.q = np.zeros(dims)
+        self.map = map_func
+
+    def policy_greedy(self, state):
+        qs = self.q[state]
+
+        if qs[0] == qs[1]:
+            return randrange(2)
+        else:
+            return qs.argmax()
+
+    def policy_epsilon(self, state):
+        if random() < epsilon:
+            return randrange(2)
+        else:
+            return self.policy_greedy(state)
+
+    def training(self, num_epochs):
+        for i in range(num_epochs):
+            state = self.env.reset()
+            if self.map is not None:
+                state = self.map(state)
+            done = False
+
+            while not done:
+                action = self.policy_epsilon(state)
+                new_state, reward, done, _ = self.env.step(action)
+                if self.map is not None:
+                    new_state = self.map(new_state)
+
+                if done:
+                    self.q[state + (action, )] += alpha * (
+                        reward - self.q[state + (action, )])
+                else:
+                    self.q[state + (action, )] += alpha * (
+                        reward + gamma * max(self.q[new_state]) -
+                        self.q[state + (action, )])
+
+                state = new_state
+
+    def evaluate(self, num_epochs):
+        wins = 0
+        draws = 0
+        losses = 0
+
+        for i in range(num_epochs):
+            state = self.env.reset()
+            if self.map is not None:
+                state = self.map(state)
+            done = False
+
+            while not done:
+                action = self.policy_greedy(state)
+                new_state, reward, done, _ = self.env.step(action)
+                if self.map is not None:
+                    state = self.map(new_state)
+
+            if reward > 0:
+                wins += 1
+            elif reward == 0:
+                draws += 1
+            else:
+                losses += 1
+
+        return wins, draws, losses
+
+
 #  - sum of cards in hand (4-21)
 #  - dealers card (1-10)
 #  - usable ace (0-1)
 #  - action (0-1)
-q = np.zeros((18,10,2,2))
+dims = (18, 10, 2, 2)
+
 
 def map_state(state):
-    return (state[0]-4, state[1]-1, int(state[2]))
+    return (state[0] - 4, state[1] - 1, int(state[2]))
 
-def policy_greedy(state):
-    qs = q[state]
-    
-    if qs[0] == qs[1]:
-        return randrange(2)
-    else:
-        return qs.argmax()
 
-def policy_epsilon(state):
-    if random() < epsilon:
-        return randrange(2)
-    else:
-        return policy_greedy(state)
-    
-def training(num_epochs):
-    for i in range(num_epochs):
-        state = map_state(env.reset())
-        done = False
-        
-        while not done:
-            action = policy_epsilon(state)
-            new_state, reward, done, _ = env.step(action)
-            new_state = map_state(new_state)
-            
-            if done:
-                q[state + (action,)] += alpha * (reward - q[state + (action,)])
-            else:
-                q[state + (action,)] += alpha * (reward + gamma*max(q[new_state]) - q[state + (action,)])
-            
-            state = new_state
-
-def evaluate(num_epochs):
-    wins = 0
-    draws = 0
-    losses = 0
-    
-    for i in range(num_epochs):
-        state = map_state(env.reset())
-        done = False
-        
-        while not done:
-            action = policy_greedy(state)
-            new_state, reward, done, _ = env.step(action)
-            state = map_state(new_state)
-            
-        if reward > 0:
-            wins += 1
-        elif reward == 0:
-            draws += 1
-        else:
-            losses += 1
-    
-    return wins, draws, losses
-        
-  
-
+env = gym.make('Blackjack-v0')
+agent = QAgent(env, dims, map_state)
